4a42ccacf6f3a26c1625ac7a4e1cfae3d3b43481
Samurzel <64125069+Samurzel@users.noreply.github.com>
2020-06-21 15:25:12 +0200

    Add files via upload

--- Aufgabe7/script.ts
+++ Aufgabe7/script.ts
@@ -6,183 +6,10 @@ interface Product {
     description: String;
     price: number;
 }
-// let originals0: Product = {
-//     img: "url(MarkI.png)",
-//     name: "Mark I",
-//     description: "The real O.G with barely the very basics to escape a group of criminals holding you hostage",
-//     price: 1000000
-// };
-
-// let originals1: Product = {
-//     img: "url(MarkII.png)",
-//     name: "Mark II",
-//     description: "The silver design of this suit really represents it’s over the top coolness factor. So much it has a slight freezing problem.",
-//     price: 1500000
-// };
-
-// let originals2: Product = {
-//     img: "url(MarkIII.png)",
-//     name: "Mark III",
-//     description: "The Mark III introduces the basic design scheme for years to come. And a bunch of weapons to use against your standard world threatening villains.",
-//     price: 2000000
-// };
-
-// let originals3: Product = {
-//     img: "url(MarkIV.png)",
-//     name: "Mark IV",
-//     description: "It’s the Mark III, but better! More strength, more speed, more weapons, more everything!",
-//     price: 2500000
-// };
-
-// let advanced0: Product = {
-//     img: "url(MarkV.png)",
-//     name: "Mark V",
-//     description: "You know what a suit needs? A suitcase to carry it whereever you want to.",
-//     price: 3000000
-// };
-
-// let advanced1: Product = {
-//     img: "url(MarkVI.png)",
-//     name: "Mark VI",
-//     description: "This suit features not only a significantly higher energy output, it even let’s you breathe underwater!",
-//     price: 3500000
-// };
-
-// let advanced2: Product = {
-//     img: "url(MarkVII.png)",
-//     name: "Mark VII",
-//     description: "We refer to it as the Lego suit. You can take each individual part of and assemble it together when you need it.",
-//     price: 4000000
-// };
-
-// let advanced3: Product = {
-//     img: "url(MarkVIII.png)",
-//     name: "Mark VIII",
-//     description: "Less bulky, more maneuvery! The Mark VIII has the flexibility you need in close combat, while protecting you better than ever before.",
-//     price: 4500000
-// };
-
-// let advanced4: Product = {
-//     img: "url(MarkIX.png)",
-//     name: "Mark IX",
-//     description: "The first Tesla of our Iron Man suits. You want to suit up? Give it a call, it’ll fly right to you.",
-//     price: 5000000
-// };
-
-// let latest0: Product = {
-//     img: "url(MarkX.png)",
-//     name: "Mark X",
-//     description: "When you want to be an extra speedy boy. Fly three times faster than the Mark XI!",
-//     price: 5500000
-// };
-
-// let latest1: Product = {
-//     img: "url(MarkXI.png)",
-//     name: "Mark XI",
-//     description: "The Mark XI introduces basic stealth capabilities in our suits. Kinda like a snake? Snake? SNAAAAAKE?!",
-//     price: 6000000
-// };
-
-// let latest2: Product = {
-//     img: "url(MarkXII.png)",
-//     name: "Mark XII",
-//     description: "Mark XII breaks a bit with our design direction, but that’s certainly not the only thing it breaks. By far.",
-//     price: 6500000
-// };
-
-// let originals: Product[] = [originals0, originals1, originals2, originals3];
-// let advanced: Product[] = [advanced0, advanced1, advanced2, advanced3, advanced4];
-// let latest: Product[] = [latest0, latest1, latest2];
-
-let originalsNew: Product[];
-let advancedNew: Product[];
-let latestNew: Product[];
 
 document.addEventListener("DOMContentLoaded", init);
-async function init(): Promise<T> {
-
-    let jsonString = JSON.stringify(await loadArtikel());
-    let objJson = JSON.parse(jsonString);
-
-    var oCount = 0;
-    var aCount = 0;
-    var lCount = 0; 
-
-    for(var i = 0; i < objJson.length; i++) {
-        
-
-        
-
-        var artikel = Object.keys(objJson);
-        
-        if (objJson[i].kategorie == "originals" && oCount == 0) {
-            oCount++;
-            document.getElementsByClassName("wrapper")[0].innerHTML = "";
-            document.getElementsByClassName("wrapper")[0].innerHTML += "<h2>Originals</h2>";
-        }
-        else if (objJson[i].kategorie == "advanced" && aCount == 0) {
-            aCount++;
-            document.getElementsByClassName("wrapper")[0].innerHTML += "<h2>Advanced</h2>";
-        }
-        else if (objJson[i].kategorie == "latest" && lCount == 0) {
-            lCount++;
-            document.getElementsByClassName("wrapper")[0].innerHTML += "<h2>Latest</h2>";
-        }
-
-        // if (objJson[i].kategorie == "originals") {
-        //     originalsNew.push(objJson[i]);
-        // }
-        // else if (objJson[i].kategorie == "advanced") {
-        //     advancedNew.push(objJson[i]);
-        // }
-        // else if (objJson[i].kategorie == "latest") {
-        //     latestNew.push(objJson[i]);
-        // }
-
-        createArticle(objJson[i]);
-        // console.log(obj.id);
-    }
-
-    // document.getElementsByClassName("wrapper")[0].innerHTML = "";
-    // document.getElementsByClassName("wrapper")[0].innerHTML += "<h2>Originals</h2>";
-
-    // let myJsonOriginals = JSON.stringify(originals);
-    // var myObjOriginals = JSON.parse(myJsonOriginals);
-    // for(var i = 0; i < myObjOriginals.length; i++) {
-
-    //     createArticle(myObjOriginals[i]);
-    //     // console.log(obj.id);
-    // }
-    // // renderProducts(originals);
-
-    // document.getElementsByClassName("wrapper")[0].innerHTML += "<h2>Advanced</h2>";
-
-    // let myJsonAdvanced = JSON.stringify(advanced);
-    // var myObjAdvanced = JSON.parse(myJsonAdvanced);
-    // for(var i = 0; i < myObjAdvanced.length; i++) {
-
-    //     createArticle(myObjAdvanced[i]);
-    //     // console.log(obj.id);
-    // }
-    // // renderProducts(advanced);
-
-    // document.getElementsByClassName("wrapper")[0].innerHTML += "<h2>Latest</h2>";
-
-    // let myJsonLatest = JSON.stringify(latest);
-    // var myObjLatest = JSON.parse(myJsonLatest);
-    // for(var i = 0; i < myObjLatest.length; i++) {
-
-    //     createArticle(myObjLatest[i]);
-    // }
-    // // renderProducts(latest);
-
-    return;
-}
-
-function renderProducts(_article: Product[]): void {
-    for (let i: number = 0; i < _article.length; i++) {
-        createArticle(_article[i]);
-    }
+function init(): void {
+    displayObjects("all");
 }
 
 function createArticle(_article: Product): void {
@@ -202,41 +29,63 @@ function createArticle(_article: Product): void {
 let cartCounter: number = 0;
 let cartValue: number = 0;
 
-
-
-
 function hndShoppingCart(_article: number): void {
-
     cartValue += _article;
     cartCounter++;
- 
-    const el = document.getElementById('cartCounter');
- 
+
+    const el = document.getElementById("cartCounter");
+
     if (el) {
-      const definitelyAnElement = el;
-   definitelyAnElement.innerHTML = cartCounter.toString();
- }
- 
+        const definitelyAnElement = el;
+        definitelyAnElement.innerHTML = cartCounter.toString();
+    }
     console.log(cartValue);
- }
+}
 
 async function loadArtikel(): Promise<JSON> {
-
-    let response: Response = await fetch('http://127.0.0.1:5500/Aufgabe7/Artikel.JSON');
+    let response: Response = await fetch("http://127.0.0.1:5500/Aufgabe7/Artikel.JSON");
     return response.json();
 }
- 
-function sortDisplay(_article: Product[], _name: string): void {
-    document.getElementsByClassName("wrapper")[0].innerHTML = "";
-    document.getElementsByClassName("wrapper")[0].innerHTML += `<h2>${_name}</h2>`;
-    renderProducts(_article);
- }
-function resetDisplay(): void {
-    document.getElementsByClassName("wrapper")[0].innerHTML = "";
-    document.getElementsByClassName("wrapper")[0].innerHTML += "<h2>Originals</h2>";
-    renderProducts(originals);
-    document.getElementsByClassName("wrapper")[0].innerHTML += "<h2>Advanced</h2>";
-    renderProducts(advanced);
-    document.getElementsByClassName("wrapper")[0].innerHTML += "<h2>Latest</h2>";
-    renderProducts(latest);
- }
+
+async function displayObjects(_kategorie: string): Promise<number>  {
+    let jsonString = JSON.stringify(await loadArtikel());
+    let objJson = JSON.parse(jsonString);
+
+    let oCount = 0;
+    let aCount = 0;
+    let lCount = 0; 
+
+    if (_kategorie == "all") {
+        for (let i = 0; i < objJson.length; i++) {
+                        
+            if (objJson[i].kategorie == "originals" && oCount == 0) {
+                oCount++;
+                document.getElementsByClassName("wrapper")[0].innerHTML = "";
+                document.getElementsByClassName("wrapper")[0].innerHTML += "<h2>Originals</h2>";
+            }
+            else if (objJson[i].kategorie == "advanced" && aCount == 0) {
+                aCount++;
+                document.getElementsByClassName("wrapper")[0].innerHTML += "<h2>Advanced</h2>";
+            }
+            else if (objJson[i].kategorie == "latest" && lCount == 0) {
+                lCount++;
+                document.getElementsByClassName("wrapper")[0].innerHTML += "<h2>Latest</h2>";
+            }
+
+            createArticle(objJson[i]);
+        }
+    } else {
+        for (let i = 0; i < objJson.length; i++) {
+            if (objJson[i].kategorie == _kategorie) {
+                if (oCount == 0) {
+                    oCount++;
+                    document.getElementsByClassName("wrapper")[0].innerHTML = "";
+                    document.getElementsByClassName("wrapper")[0].innerHTML += `<h2>${_kategorie.replace(/^./, _kategorie[0].toUpperCase())}</h2>`;
+                }
+                createArticle(objJson[i]);
+            }
+        }
+    }
+
+    return 0;
+}
